d838bed4dd4098aa080e0f71c882017b5f9ab1b4
LucaElisio <personal.elix@gmail.com>
2024-04-08 21:23:28 +0200

    feat: milestone 1

---
+++ README.md
@@ -0,0 +1,8 @@
+**Milestone 1**
+1. aggiungere event listener a tutti i numeri (0-9)
+    - [ ] Cerco l'elemento numbers in pagina (numbersElem)
+    - [ ] creo ciclo for che scorre tutti i numeri
+        - i = 1; i <= 10; i++
+        - Cerco bottone in pagina (document.querySelector(".numbers button:nth-child(i)"));
+2. quando si clicca su un numero, concatenarlo al numero
+    - [ ] Concateno il valore del bottone corrente trasformato in stringa ad una variabile
---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Attenzione: La nostra calcolatrice deve fare dei calcoli tra due numeri interi. Quindi l'utente inserisce le cifre del primo numero, dopo dovrà cliccare sull'operazione, poi inserirà le cifre del secondo numero e infine dovrà cliccare sul tasto = per vedere il risultato. Non si può applicare più di un'operazione alla volta.
+// MILESTONE 1
+// aggiungere event listener a tutti i numeri (0-9)
+// quando si clicca su un numero, concatenarlo al numero visualizzato in alto
+// MILESTONE 2
+// aggiungere event listener a tutti gli operatori
+// quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
+// MILESTONE 3
+// aggiungere event listener al pulsante =
+// quando si clicca su =, salvare il secondo operando, effetuare il calcolo corretto in base all'operatore selezionato e visualizzare il risultato in alto
+// MILESTONE 4
+// pulsante canc: cliccando il pulsante si resetta il calcolo
+// :stella2: BONUS
+// verificare che non si stia cercando di dividere per 0
+
+
+// Dichiaro variabile globale per il risultato
+let result = 0;
+// Dichiaro variabile globale per il primo numero
+let num1 = "";
+// Dichiaro variabile globale per il secondo numero
+let num2;
+
+// Inserisco il risultato in pagina
+// Trovo elemento result in pagina
+const resultElem = document.querySelector(".result");
+// Stampo il risultato in pagina
+resultElem.innerHTML = result;
+
+// Aggiungo event listener a tutti i numeri
+// Cerco elemento numbers in pagina
+const numbersElem = document.querySelector(".numbers");
+console.log(numbersElem);
+
+for (let i = 1; i <= 10; i++) {
+
+    // Cerco bottone numerico in pagina e salvo il valore in variabile
+    const numElem = document.querySelector(`.numbers button:nth-child(${i})`);
+    console.log(numElem);
+
+    // Al click, concateno il valore del bottone cliccato in stringa in una variabile
+    numElem.addEventListener("click", function() {
+        // Salvo il valore del bottone corrente
+        const numElemValue = numElem.innerHTML;
+        num1 += numElemValue.toString();
+        console.log(num1);
+        
+        // Stampo il risultato sul display
+        resultElem.innerHTML = num1;
+    })
+
+}
+
